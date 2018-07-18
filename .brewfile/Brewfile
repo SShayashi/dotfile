ba80e782198930ca202602e65bc1d11397bbaab3
hayashi <sshayashi0208@gmail.com>
2018-07-18 10:25:24 +0900

    fix

---
+++ .brewfile/Brewfile
@@ -0,0 +1,1 @@
+git SShayashi/Brewfile
---
+++ .brewfile/installBrew.sh
@@ -0,0 +1,10 @@
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update
+brew install git
+git clone https://github.com/SShayashi/dotfile.git
+brew tap caskroom/cask
+brew install brew-cask
+#then setup git 
+
+#brew-file set_repo
+#brwe file install
